a03411af234dc2ddad52404e6a9641179db13d67
Tharindu Jayashan <tharindujayashan421@gmail.com>
2025-02-27 16:56:51 +0530

    Add theme variables for primary, secondary, and accent colors

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom"
 
 export default function Header(){
     return(
-        <header className="w-full h-[100px] shadow-xl flex justify-center items-center relative">
+        <header className="w-full h-[100px] shadow-xl flex justify-center items-center relative bg-accent">
 
-            <img src="/Audiologo.jpg" alt="Logo" 
+            <img src="/audio-logo.png" alt="Logo" 
             className="w-[100px] h-[100px] object-cover boader-[3px] left-1 rounded-full absolute "/>
 
         <Link to = "/" className= "text-[25px] font-bold m-1">
